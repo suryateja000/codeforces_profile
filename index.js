3c9a25322a5e8f0c8fe2c06f92f8c057d16d9ee6
Raghavnadiminti <raghavnadiminti@gmail.com>
2025-06-15 11:53:15 +0530

    index.js created and configurations

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { connectDB, mongoose } = require('./config/db'); 
+
+app.use(cors({
+  origin: '*',
+  methods: ['POST', 'GET'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true
+}));
+app.use(express.json());
+
+
+connectDB();
+
+const PORT =  5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
